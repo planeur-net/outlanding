4a2a1c8f3dd173545ea921b2a4f5f21096071e9c
Ludovic Launer <ludovic.launer@gmail.com>
2024-02-03 18:00:25 +0100

    wip

---
+++ src/WaypointProcessor/WaypointProcessor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using CommandLine;
+using WaypointProcessor.Services;
+
+namespace QuickStart
+{
+    class Program
+    {
+        [Verb("duplicates", HelpText = "Add file contents to the index.")]
+        class DuplicatesOptions
+        {
+            [Option('b', "baseFileName", Required = true, HelpText = "Base file name")]
+            public string? BaseFileName { get; set; }
+
+            [Option('c', "comparedFileName", Required = true, HelpText = "Compared file name")]
+            public string? ComparedFileName { get; set; }
+        }
+
+
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            CommandLine.Parser.Default.ParseArguments<DuplicatesOptions>(args)
+                .WithParsed(program.RemoveDuplicates)
+                .WithNotParsed(HandleParseError);
+        }
+
+        private static void HandleParseError(IEnumerable<Error> errs)
+        {
+        }
+
+
+
+        private void RemoveDuplicates(DuplicatesOptions options)
+        {
+            var parser = new CsvFileParser();
+            parser.parseFile(options.BaseFileName);
+
+        }
+
+    }
+}
---
+++ src/WaypointProcessor/WaypointProcessor/Services/CsvFileParser.cs
@@ -0,0 +1,44 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaypointProcessor.Models;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WaypointProcessor.Services
+{
+    /// <summary>
+    /// CSV file Parser.
+    /// </summary>
+    internal class CsvFileParser
+    {
+        private List<WaypointModel> _waypoints;
+
+        public void parseFile(string filename)
+        {
+            _waypoints = new List<WaypointModel>();
+
+            // Configure CSV parser
+            var config = new CsvHelper.Configuration.CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture)
+            {
+                MissingFieldFound = null,
+                HasHeaderRecord = true,
+                Delimiter = ",",
+                ShouldSkipRecord = args => args.Row[0].Contains("version=")
+            };
+
+
+
+            using (var reader = new StreamReader(filename))
+            using (var csv = new CsvReader(reader, config))
+            {
+                csv.Context.RegisterClassMap<WaypointModelMap>();
+                _waypoints = csv.GetRecords<WaypointModel>().ToList();
+            }
+        }
+    }
+}
